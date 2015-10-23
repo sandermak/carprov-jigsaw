e12f9af87753e9e63bbf6761c255071e3a734e7d
Sander Mak <sander.mak@luminis.eu>
2015-10-23 16:13:55 +0200

    Jar command changed for b83

--- jar-run.sh
+++ jar-run.sh
@@ -1,8 +1,8 @@
 ./build.sh
 mkdir mods/jarred
-jar --create --archive=mods/jarred/carprov.dashboard.api@1.0.jar --module-version=1.0 -C mods/carprov.dashboard.api .
-jar --create --archive=mods/jarred/carprov.music@1.0.jar --module-version=1.0 -C mods/carprov.music .
-jar --create --archive=mods/jarred/carprov.phone@1.0.jar --module-version=1.0 -C mods/carprov.phone .
-jar --create --archive=mods/jarred/carprov.navigation@1.0.jar --module-version=1.0 -C mods/carprov.navigation .
-jar --create --archive=mods/jarred/carprov.dashboard.jfx@1.0.jar --module-version=1.0 --main-class=carprov.dashboard.jfx.Dashboard -C mods/carprov.dashboard.jfx .
+jar --create --file mods/jarred/carprov.dashboard.api@1.0.jar --module-version=1.0 -C mods/carprov.dashboard.api .
+jar --create --file mods/jarred/carprov.music@1.0.jar --module-version=1.0 -C mods/carprov.music .
+jar --create --file mods/jarred/carprov.phone@1.0.jar --module-version=1.0 -C mods/carprov.phone .
+jar --create --file mods/jarred/carprov.navigation@1.0.jar --module-version=1.0 -C mods/carprov.navigation .
+jar --create --file mods/jarred/carprov.dashboard.jfx@1.0.jar --module-version=1.0 --main-class=carprov.dashboard.jfx.Dashboard -C mods/carprov.dashboard.jfx .
 java -mp mods/jarred -m carprov.dashboard.jfx
